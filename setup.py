d4805531348eed445b8c65dc3c68106f228a11d7
JoshDaBosh <jwanggt@gmail.com>
2018-07-21 02:55:54 +0000

    Fixed function top10_of_day

--- setup.py
+++ setup.py
@@ -13,6 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/joshdabosh/webtoons",
     packages=setuptools.find_packages(),
+    install_requires=(
+        "bs4",
+        "requests",
+        "lxml"
+    ),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

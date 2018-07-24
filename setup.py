63dfc0185265ea9d0c4dcf5138c1bf562ee857ae
JoshDaBosh <32721284+joshdabosh@users.noreply.github.com>
2018-07-23 21:03:14 -0600

    git cli would have been less work
    but i was too lazy

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=(
         "bs4",
-        "requests",
-        "lxml"
+        "requests"
     ),
     classifiers=(
         "Programming Language :: Python :: 3",
